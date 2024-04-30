2a72129e7e02a461c60127906f98d774670e24a9
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-04-30 11:06:18 +0200

    add promise to confirm function, rename functions, update README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# alert
-A simple script for displaying a message to user.
+# Pop!
+A simple script for displaying messages, confirming actions, and getting input from users.
+
+## Example usage
+
+``` simplePop("success", "Welcome new user!") //can be closed by the user ```
+
+``` const bool = confirmPop("Are you sure you want to delete this?") //returns true or false ```
+
+``` const name = inputPop("What's your name?") //returns user input as string ```
--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-function simpleAlert(type, message) {
+function closePop(element) {
+    element.parentNode.remove();
+}
+
+function simplePop(message) {
     const body = document.querySelector("body");
     const alert = document.createElement("div");
     alert.classList.add("success-div");
-    alert.innerHTML = `<p>${message}</p><p class="close" onclick="return this.parentNode.remove();">x</p>`;
+    alert.innerHTML = `<p>${message}</p><p class="close" onclick="closePop(this)">x</p>`;
     body.appendChild(alert);
 }
 
-async function confirm(message) {
-    const body = document.querySelector("body");
-    const alert = document.createElement("div");
-    alert.classList.add("confirm");
-    alert.innerHTML = `<p>${message}</p><p>Cancel</p><p>Yes</p>`;
-    body.appendChild(alert);
+async function confirmPop(message) {
+    return new Promise((resolve, reject) => {
+        const body = document.querySelector("body");
+        const alert = document.createElement("div");
+        alert.classList.add("confirm");
+        alert.innerHTML = `<p>${message}</p><div class="buttonGroup"></div><button class="cancelButton">Cancel</button><button class="yesButton">Yes</button>`;
+        body.appendChild(alert);
+
+        document.querySelector(".cancelButton").addEventListener("click", () => {
+            alert.remove();
+            resolve(false);
+        });
+
+        document.querySelector(".yesButton").addEventListener("click", () => {
+            alert.remove();
+            resolve(true);
+        });
+    });
 }
