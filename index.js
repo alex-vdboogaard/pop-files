a31a84b5fb6c0c198f719eb5134beb9bb79c6aef
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-04-30 12:58:09 +0200

    remove alerts and improve popup styles

--- index.js
+++ index.js
@@ -2,20 +2,50 @@ function closePop(element) {
     element.parentNode.remove();
 }
 
-function simplePop(message) {
-    const body = document.querySelector("body");
-    const alert = document.createElement("div");
-    alert.classList.add("success-div");
-    alert.innerHTML = `<p>${message}</p><p class="close" onclick="closePop(this)">x</p>`;
-    body.appendChild(alert);
+function closeAllPop() {
+    document.querySelectorAll(".pop").forEach(pop => {pop.remove();});
 }
 
+//this makes the popup move up out of the screen then disappear
+function delayedRemove() {
+    const popElements = document.querySelectorAll(".pop");
+    setTimeout(function() {
+        popElements.forEach(pop => {
+            pop.style.transition = "margin-top 0.5s ease-out";
+            pop.style.marginTop = "-80px"; 
+            pop.addEventListener("transitionend", function() {
+                pop.remove();
+            }, { once: true });
+        });
+    }, 3000);
+}
+
+
+
+function simplePop(message) {
+    closeAllPop();
+    const body = document.querySelector("body");
+    const alert = document.createElement("div");
+    alert.classList.add("top-pop", "pop", "success-div");
+    alert.innerHTML = `<p>${message}</p><p class="close" onclick="closePop(this)">x</p>`;
+    body.appendChild(alert);
+
+    setTimeout(function() {
+        alert.style.transition = "top 0.5s ease-out";
+        alert.style.top = "10px";
+    }, 100); 
+
+    delayedRemove();
+}
+
+
 async function confirmPop(message) {
+    closeAllPop();
     return new Promise((resolve, reject) => {
         const body = document.querySelector("body");
         const alert = document.createElement("div");
-        alert.classList.add("confirm");
-        alert.innerHTML = `<p>${message}</p><div class="buttonGroup"></div><button class="cancelButton">Cancel</button><button class="yesButton">Yes</button>`;
+        alert.classList.add("pop","confirm");
+        alert.innerHTML = `<p>${message}</p><div class="buttonGroup"><button class="btn cancelButton">Cancel</button><button class="btn yesButton">Yes</button></div>`;
         body.appendChild(alert);
 
         document.querySelector(".cancelButton").addEventListener("click", () => {
