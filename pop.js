86f4b4ed015a5ee5400f8c52d6198404ca918552
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-05-03 09:12:37 +0200

    add icons to popups

--- pop.js
+++ pop.js
@@ -20,14 +20,33 @@ function delayedRemove() {
     }, 3000);
 }
 
-function simplePop(type, message) {
+function simplePop(type, message, position="top") {
     closeAllPop();
     if (type === "success") type = "pop-success";
     if (type === "error") type = "pop-error";
     const body = document.querySelector("body");
     const alert = document.createElement("div");
     alert.classList.add("pop-top", "pop", type);
-    alert.innerHTML = `<p>${message}</p><p class="pop-close" onclick="closePop(this)">x</p>`;
+
+    if (type === "pop-success") {
+        alert.innerHTML = `<svg class="pop-svg" style="margin-right:15px" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 7L10 17L5 12" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <p>${message}</p>`;
+    }
+    else {
+        alert.innerHTML = `<svg style="margin-right:15px" class="pop-svg" width="90" height="90" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_2287_12)">
+        <path d="M45 0C20.1445 0 0 20.1445 0 45C0 69.8555 20.1445 90 45 90C69.8555 90 90 69.8555 90 45C90 20.1445 69.8555 0 45 0ZM11.25 45C11.25 26.3496 26.3672 11.25 45 11.25C52.4004 11.25 59.2383 13.6582 64.793 17.7012L17.7012 64.793C13.6582 59.2383 11.25 52.4004 11.25 45ZM45 78.75C37.5996 78.75 30.7617 76.3418 25.207 72.2988L72.2988 25.207C76.3418 30.7793 78.75 37.5996 78.75 45C78.75 63.6504 63.6328 78.75 45 78.75Z" fill="white"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_2287_12">
+        <rect width="90" height="90" fill="white"/>
+        </clipPath>
+        </defs>
+        </svg><p>${message}</p>`;
+    }
+    
     body.appendChild(alert);
 
     setTimeout(function() {
